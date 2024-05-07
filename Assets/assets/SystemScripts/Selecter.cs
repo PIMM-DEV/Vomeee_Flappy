1b17c3ec331bb4752a0786709d4157e86ad51778
Vomeee <158576443+Vomeee@users.noreply.github.com>
2024-05-07 23:21:54 +0900

    간단한 ui

---
+++ Assets/assets/SystemScripts/Selecter.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selecter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public RectTransform imageTransform;
+    public Vector2 newPosition;
+
+    int[] menuY = new int[3] { 85, 0, -85 };
+
+
+    void Start()
+    {
+        int currPosition = 1;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
