269528d2b64c939cb6d411ea3359c14743ca50c7
Vomeee <ye43019@gmail.com>
2024-05-09 11:38:36 +0900

    해병 UI

--- Assets/assets/SystemScripts/Enemy_Act.cs
+++ Assets/assets/SystemScripts/Enemy_Act.cs
@@ -15,4 +15,6 @@ public class Enemy_Act : MonoBehaviour
     {
         
     }
+
+
 }
--- Assets/assets/SystemScripts/Selecter.cs
+++ Assets/assets/SystemScripts/Selecter.cs
@@ -5,21 +5,46 @@ using UnityEngine;
 public class Selecter : MonoBehaviour
 {
     // Start is called before the first frame update
-    public RectTransform imageTransform;
+    public RectTransform recttransform;
     public Vector2 newPosition;
+    int currselection;
 
-    int[] menuY = new int[3] { 85, 0, -85 };
+    int[] posYs = new int[3] { 85, 0, -85 };
 
 
     void Start()
     {
-        int currPosition = 1;
+        currselection = 0;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (currselection == 0) { }
+            else
+            {
+                currselection--;
+                recttransform.anchoredPosition = new Vector2(-150, posYs[currselection]);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (currselection == 2) { }
+            else
+            {
+                currselection++;
+                recttransform.anchoredPosition = new Vector2(-150, posYs[currselection]);
+            }
+
+        }
+
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+
+        }
     }
 }
